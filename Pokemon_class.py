bfd9eab469194554f72eb25b517ef9c164a5ce66
AlexSnix <100698745+AlexSnix@users.noreply.github.com>
2022-06-07 09:47:58 -0400

    Add files via upload

--- Pokemon_class.py
+++ Pokemon_class.py
@@ -1,9 +1,12 @@
 class Pokemon:
 
-    def __init__(self,name,type,stats,moves):
+    def __init__(self,index,name,type,hp,attack,defence,moves):
+        self.index = index
         self.name = name
         self.type = type
-        self.stats = stats
+        self.hp = hp
+        self.attack = attack
+        self.defence = defence
         self.moves = moves
 
     def get_name(self):
@@ -12,8 +15,14 @@ class Pokemon:
     def get_type(self):
         return self.type
     
-    def get_stats(self):
-        return self.stats
+    def get_hp(self):
+        return self.hp
+
+    def get_attack(self):
+        return self.attack
+
+    def get_defence(self):
+        return self.defence
 
     def get_moves(self):
         return self.moves
