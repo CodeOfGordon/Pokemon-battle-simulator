36010d06347ff643c0e4fb83484f865737500db5
CodingNoobGordon <81336844+CodingNoobGordon@users.noreply.github.com>
2022-06-08 11:09:49 -0400

    Worked on windows class
    - Found out tkinter is origin is on top left corner (whyy)
    - Changed .grid() -> .place()

--- GamePokemon_class.py
+++ GamePokemon_class.py
@@ -6,8 +6,13 @@ from Fight_class import *
 from Player_class import *
 import random
 
+
 MENU_WIDTH = 1024
 MENU_HEIGHT = 576
+MENU_CENTERX = MENU_WIDTH / 2
+MENU_CENTERY = MENU_HEIGHT / 2
+BOTTOM_OPTIONS = MENU_HEIGHT / 4
+
 
 class GamePokemon:
 
@@ -49,37 +54,38 @@ class GamePokemon:
     def test(self):
         COLORS = ["red", "green", "blue", "yellow"]
         example = Label(self.root, text="test", background=random.choice(COLORS))
-        example.place(x=MENU_WIDTH-50, y=MENU_HEIGHT-50, width=50, height=50)
+        example.place(x=0, y=0, width=50, height=50)
         
 
     
-    def setup(self): # Change to use place() instead of grid()
+    def setup(self): # Change to use place() instead of grid(). Also origin is top left corner
         '''Create all of the widgets for the menu.'''
         self.font = Font(family="Helvetica", size=20)
 
         self.pokemon = Label(self.root)
-        self.pokemon.grid(row=0, column=0, columnspan=2)
-
-        self.run_button = Button(self.root, text="Run", font=self.font, command=self.test)
-        self.run_button.grid(row=1, column=0)
+        self.pokemon.place(x=MENU_CENTERX/2,y=BOTTOM_OPTIONS)
 
         self.fight_button = Button(self.root, text="Fight", font=self.font, command=self.test)
-        self.fight_button.grid(row=1, column=1)
-
-        self.switch_button = Button(self.root, text="Switch", font=self.font, command=self.test)
-        self.switch_button.grid(row=2, column=0)
-
-        self.pokemon_name = Label(self.root, text="dog",font=self.font) # Insert pokemon name variable
-        self.pokemon_name.grid(row=2, column=1)
-
-        self.pokemon_hp = Frame(self.root)
-        self.pokemon_hp.grid(row=3, column=0)
+        self.fight_button.place(x=MENU_CENTERX,y=BOTTOM_OPTIONS*0.9,width=MENU_CENTERX/4,height=BOTTOM_OPTIONS/4)
 
         self.bag_button = Button(self.root, text="Bag", font=self.font, command=self.test)
-        self.bag_button.grid(row=3, column=1)
+        self.bag_button.place(x=MENU_CENTERX*1.8,y=BOTTOM_OPTIONS*0.9,width=MENU_CENTERX/4,height=BOTTOM_OPTIONS/4)
+
+        self.switch_button = Button(self.root, text="Switch", font=self.font, command=self.test)
+        self.switch_button.place(x=MENU_CENTERX,y=BOTTOM_OPTIONS-(BOTTOM_OPTIONS/4),width=MENU_CENTERX/4,height=BOTTOM_OPTIONS/4)
+
+        self.run_button = Button(self.root, text="Run", font=self.font, command=self.test)
+        self.run_button.place(x=MENU_CENTERX*1.8,y=BOTTOM_OPTIONS-(BOTTOM_OPTIONS/4),width=MENU_CENTERX/4,height=BOTTOM_OPTIONS/4)
+
+        self.pokemon_name = Label(self.root, text="placeholder",font=self.font) # Insert pokemon name variable
+        self.pokemon_name.place(x=MENU_CENTERX*1.2,y=BOTTOM_OPTIONS*1.5,width=MENU_CENTERX/4,height=BOTTOM_OPTIONS/4)
+
+
+        '''self.pokemon_hp = Frame(self.root)
+        self.pokemon_hp.grid(row=3, column=0)
 
         self.pokemon_remaining = Frame(self.root)
-        self.pokemon_remaining.grid(row=4, column=0)
+        self.pokemon_remaining.grid(row=4, column=0)'''
 
 
 if __name__ == '__main__':
